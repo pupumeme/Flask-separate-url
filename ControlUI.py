bc131bc7229b26d85591f652a860f984d082ae23
pupumeme <pupumeme@users.noreply.github.com>
2019-08-13 13:51:57 +0800

    separate url

--- ControlUI.py
+++ ControlUI.py
@@ -1,23 +1,17 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, url_for
 import os
 
 app=Flask(__name__)
-infos=[]
 basedir = os.path.abspath(os.path.dirname(__file__))
-fname = ""
+infos = []
+global fname
+fname = ''
 status = {}
 
 @app.route("/", methods=['GET', 'POST'])
 def control():
-    if request.method == "POST":
-        img = request.files.get('photo')
-        path = basedir+"\\static\\photo\\"
-        fname = img.filename
-        file_path = path+fname
-        img.save(file_path)
-        return render_template("ControlUI.html", imgName=fname, status=status)
-
-    return render_template("ControlUI.html", imgName="", status=status)
+    global fname
+    return render_template("ControlUI.html", imgName=fname, status=status)
     
 @app.route("/infos", methods=['GET', 'POST'])
 def info():
@@ -28,8 +22,19 @@ def info():
             status[request.form["value"]] = False
         else:
             status[request.form["value"]] = True
-    return render_template("infos.html", infos=infos)
+    return redirect(url_for("control"))
+    # return render_template("infos.html", infos=infos)
 
+@app.route("/image", methods=['GET', 'POST'])
+def image():
+    if request.method == "POST":
+        img = request.files.get('photo')
+        global fname
+        fname = img.filename
+        if fname != '':
+            path = os.path.join(basedir, "static", "photo", fname)
+            img.save(path)
+    return redirect(url_for("control"))
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(port=8080, debug=True)
